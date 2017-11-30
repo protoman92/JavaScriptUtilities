2fe507b36a5d25a9b5cc89defd7210cc67e2593f
Hai Pham <viethai.pham@holmusk.com>
2017-12-01 00:31:28 +0800

    Added null type to Nullable<T>

--- src/type/typealias.ts
+++ src/type/typealias.ts
@@ -2,7 +2,7 @@ import { Observable } from 'rxjs';
 import { MaybeConvertibleType, TryConvertibleType } from './../functional';
 
 /// Represent a indexable key-value JS object.
-export type Nullable<T> = T | undefined;
+export type Nullable<T> = T | undefined | null;
 export type JSObject<T> = { [key: string] : Nullable<T> };
 export type TryResult<T> = T | MaybeConvertibleType<T> | TryConvertibleType<T>;
 export type ReactiveResult<T> = TryResult<T> | Observable<TryResult<T>>;
