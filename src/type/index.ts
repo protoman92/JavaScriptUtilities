476f853d6fe08387d24e2a82333aae9d0a727fac
Hai Pham <swiften.svc@gmail.com>
2018-08-30 12:21:53 +0800

    feat(typealias): add SameValueType typealias

--- src/type/index.ts
+++ src/type/index.ts
@@ -1,17 +1,3 @@
 import * as Types from './util';
-
-export {
-  IGNORE,
-  Ignore,
-  Indeterminate,
-  IndeterminateKV,
-  JSObject,
-  Nullable,
-  NullableKV,
-  Omit,
-  Return,
-  Throwable,
-  TryResult,
-} from './typealias';
-
+export * from './typealias';
 export {Types};
