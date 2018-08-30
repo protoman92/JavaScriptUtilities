fae443f5b9d04b4c139671db6eaac8a3cec1f7ca
Hai Pham <swiften.svc@gmail.com>
2018-08-30 12:11:32 +0800

    refactor(export): update index.ts to export all

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,10 @@
-export { BuildableType, BuilderType } from './buildable';
-export { Booleans } from './boolean';
-export { Collections } from './collection';
-export { Dates } from './date';
-export { Errors } from './error';
-
-export {
-  Maybe,
-  MaybeConvertibleType,
-  MaybeMap,
-  MaybeFlatMap,
-  Try,
-  TryConvertibleType,
-  TryMap,
-  TryFlatMap
-} from './functional';
-
-export { Numbers } from './number';
-export { Objects } from './object';
-
-export {
-  IGNORE,
-  Ignore,
-  Indeterminate,
-  IndeterminateKV,
-  JSObject,
-  Omit,
-  Nullable,
-  NullableKV,
-  Return,
-  TryResult,
-  Types,
-} from './type';
-
-export { Strings } from './string';
+export * from './boolean';
+export * from './buildable';
+export * from './collection';
+export * from './date';
+export * from './error';
+export * from './functional';
+export * from './number';
+export * from './object';
+export * from './string';
+export * from './type';
